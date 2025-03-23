fa80cfbe03671bbfbe27b4c0df0e017fc1910285
Detims <2005.nnhan@gmail.com>
2025-03-22 21:14:56 -0700

    include flask

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 'use client'
 import Header from './components/Header';
 import Timer from './components/Timer';
+import Link from 'next/Link';
 
 export default function Home() {
   return (
     <div>
+      <Link href='/api/index.py'></Link>
       <Header />
       <Timer />
     </div>
