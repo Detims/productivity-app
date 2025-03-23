4842b2bd6fc8a841fb95926c9a536041a12ba116
Detims <2005.nnhan@gmail.com>
2025-03-22 22:23:17 -0700

    add noise

--- assets/assets.js
+++ assets/assets.js
@@ -1,5 +1,7 @@
 import hikari from './Hikari_Icon.webp';
+import alarm from './alarm_emergencymeeting.mp3';
 
 export const assets = {
     hikari,
+    alarm,
 };
--- next.config.js
+++ next.config.js
@@ -11,6 +11,18 @@ const nextConfig = {
         },
       ]
     },
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.(mp4|mp3|ogv|oga|webm)$/i,
+        type: 'asset/resource',
+        generator: {
+          emit: true,
+          filename: (options.isServer ? '../' : '') + 'static/media/[name].[contenthash:8][ext]',
+          publicPath: (config.assetPrefix ?? config.basePath ?? '') + '/_next/static/media/',
+        },
+      });
+      return config;
+    },
   }
   
   module.exports = nextConfig
